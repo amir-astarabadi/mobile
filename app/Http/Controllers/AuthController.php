816361f4df219c0b6a91e0843e12c3c7d58056d3
amir_pourastarabadi <amir.astarabadi.dev@gmail.com>
2024-10-18 12:21:19 +0330

    imp login route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -20,5 +23,19 @@ class AuthController extends Controller
         return response([
             'token' => $token
         ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::whereEmail($request->validated('email'))->first();
+
+        if ($user && Hash::check($request->validated('password'), $user->password)) {
+            $token = $user->createToken('')->plainTextToken;
+            return response([
+                'token' => $token
+            ]);
+        }
+
+        return response(['message' => 'invalid credentials'], Response::HTTP_UNAUTHORIZED);
     }
 }
